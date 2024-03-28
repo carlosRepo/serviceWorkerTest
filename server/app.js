62409c2096cbbabf88c784c080e64768ded88773
carlos aguirre <carlospoqw@gmail.com>
2024-03-28 12:59:06 -0300

    a

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const webpush = require('web-push');
+const cors = require("cors")
+
+const port = 3000;
+
+const apiKeys = {
+    publicKey: "BEBFFsXHqgfoAwWbrUWlArMwPoFytZkZPoxIiJEoxDcGk7DvhEokMtwSiJiUY3p-TuQIyJW-jdcov1L1UMrCizk",
+    privateKey: "PkflFKJRpDX9u3MAidaa-oOyY0t-aKEpdZmmWk_L3hM"
+}
+
+webpush.setVapidDetails(
+    'mailto:YOUR_MAILTO_STRING',
+    apiKeys.publicKey,
+    apiKeys.privateKey
+)
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+const subDatabse = [];
+
+
+app.post("/save-subscription", (req, res) => {
+    console.log(req.body)
+    subDatabse.push(req.body);
+    res.json({ status: "Success", message: "Subscription saved!" })
+})
+
+app.get("/send-notification", (req, res) => {
+    webpush.sendNotification(subDatabse[0], "Hello world");
+    res.json({ "statue": "Success", "message": "Message sent to push service" });
+})
+
+app.listen(port, () => {
+    console.log("Server running on port 3000!");
+})
